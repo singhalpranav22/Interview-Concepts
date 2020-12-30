71f32aa1b656d4090e15d96119e509b34c05e5a3
Pranav Singhal <iit2019050@iiita.ac.in>
2020-12-30 15:49:25 +0530

    Added in leetcode

---
+++ LeetCode/Containerwithmostwater.cpp
@@ -0,0 +1,50 @@
+// Two Pointer approach
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+
+// Notice that you may not slant the container.
+
+ 
+
+// Example 1:
+
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+// Example 2:
+
+// Input: height = [1,1]
+// Output: 1
+// Example 3:
+
+// Input: height = [4,3,2,1,4]
+// Output: 16
+// Example 4:
+
+// Input: height = [1,2,1]
+// Output: 2
+ 
+
+// Constraints:
+
+// n = height.length
+// 2 <= n <= 3 * 104
+// 0 <= height[i] <= 3 * 104
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size()-1;
+        int l=0;
+        int r=n;
+        int ans=-1;
+        while(l<r)
+        {
+          int mn=min(height[l],height[r]);
+            ans=max(ans,(r-l)*(mn));
+            if(mn==height[l]) l++;
+            else r--;
+        }
+        return ans;
+    }
+};
