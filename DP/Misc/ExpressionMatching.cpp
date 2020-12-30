85aca783f89ebca51be832f811dc2b6c90a3beec
Pranav Singhal <iit2019050@iiita.ac.in>
2020-12-30 15:30:49 +0530

    Added expression mathcing

---
+++ DP/Misc/ExpressionMatching.cpp
@@ -0,0 +1,60 @@
+// Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*' where: 
+
+// '.' Matches any single character.​​​​
+// '*' Matches zero or more of the preceding element.
+// The matching should cover the entire input string (not partial).
+
+ 
+
+// Example 1:
+
+// Input: s = "aa", p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+// Example 2:
+
+// Input: s = "aa", p = "a*"
+// Output: true
+// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+// Example 3:
+
+// Input: s = "ab", p = ".*"
+// Output: true
+// Explanation: ".*" means "zero or more (*) of any character (.)".
+// Example 4:
+
+// Input: s = "aab", p = "c*a*b"
+// Output: true
+// Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+// Example 5:
+
+// Input: s = "mississippi", p = "mis*is*p*."
+// Output: false
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 20
+// 0 <= p.length <= 30
+// s contains only lowercase English letters.
+// p contains only lowercase English letters, '.', and '*'.
+// It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+ // This is recursive coding only, can be converted to dp
+ class Solution {
+public:
+    bool solve(string s, string p,int i,int j,int n,int m){
+        if(j>=m) return i>=n; // Base Case
+        bool fMatch = (i<n && (s[i]==p[j] || p[j]=='.' ));
+        if(j<=m-2 && p[j+1]=='*')
+        {
+            return ((fMatch&&solve(s,p,i+1,j,n,m)) || solve(s,p,i,j+2,n,m));
+        }
+        else{
+            return (fMatch && solve(s,p,i+1,j+1,n,m));
+        }
+    }
+    bool isMatch(string s, string p) {
+        int n=s.length(),m=p.length();
+        return solve(s,p,0,0,n,m);
+    }
+};
