2888ba159adf5826f94cfdc8ca1c1a49846f37db
Pranav Singhal <iit2019050@iiita.ac.in>
2021-01-01 16:35:23 +0530

    Added in LeetCode

---
+++ LeetCode/MergeTwoSortedLists.cpp
@@ -0,0 +1,44 @@
+// Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+ 
+
+// Example 1:
+
+
+// Input: l1 = [1,2,4], l2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: l1 = [], l2 = []
+// Output: []
+// Example 3:
+
+// Input: l1 = [], l2 = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both l1 and l2 are sorted in non-decreasing order.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// Very Clever Recusrion
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        if (!a || b && a->val > b->val) swap(a, b);
+    if (a) a->next = mergeTwoLists(a->next, b);
+    return a;
+    }
+};
