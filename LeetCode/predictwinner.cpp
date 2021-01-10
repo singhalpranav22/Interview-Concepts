9c811646d19259c83026690f9f5673d648a60c6f
Pranav Singhal <iit2019050@iiita.ac.in>
2021-01-11 01:11:45 +0530

    Added in Leetcode

---
+++ LeetCode/predictwinner.cpp
@@ -0,0 +1,59 @@
+// Given an array of scores that are non-negative integers. Player 1 picks one of the numbers from either end of the array followed by the player 2 and then player 1 and so on. Each time a player picks a number, that number will not be available for the next player. This continues until all the scores have been chosen. The player with the maximum score wins.
+
+// Given an array of scores, predict whether player 1 is the winner. You can assume each player plays to maximize his score.
+
+// Example 1:
+
+// Input: [1, 5, 2]
+// Output: False
+// Explanation: Initially, player 1 can choose between 1 and 2. 
+// If he chooses 2 (or 1), then player 2 can choose from 1 (or 2) and 5. If player 2 chooses 5, then player 1 will be left with 1 (or 2). 
+// So, final score of player 1 is 1 + 2 = 3, and player 2 is 5. 
+// Hence, player 1 will never be the winner and you need to return False.
+ 
+
+// Example 2:
+
+// Input: [1, 5, 233, 7]
+// Output: True
+// Explanation: Player 1 first chooses 1. Then player 2 have to choose between 5 and 7. No matter which number player 2 choose, player 1 can choose 233.
+// Finally, player 1 has more score (234) than player 2 (12), so you need to return True representing player1 can win.
+ 
+
+// Constraints:
+
+// 1 <= length of the array <= 20.
+// Any scores in the given array are non-negative integers and will not exceed 10,000,000.
+// If the scores of both players are equal, then player 1 is still the winner.
+
+class Solution {
+public:
+    int dp[100][100];
+    int solve(vector<int> a,int i,int j,int f) // function calculates that how much max the first person starting the game can score by maximising his score and minimising the score of the opponent.
+    {
+      if(i>j) 
+      {
+          return 0;
+      }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(f==0) // player -1 turn
+        {
+            f=1;
+            return dp[i][j]=max(a[i]+solve(a,i+1,j,f),a[j]+solve(a,i,j-1,f));
+        }
+        else // player -2 turn
+        {
+            f=0;
+            return dp[i][j]=min(solve(a,i+1,j,f),solve(a,i,j-1,f));
+        }
+    }
+    bool PredictTheWinner(vector<int>& a) {
+        int n=a.size();
+        memset(dp,-1,sizeof(dp));
+        int first= solve(a,0,n-1,0); // max score of player -1 
+        int sum=0;
+        for(int i=0;i<n;i++) sum+=a[i]; // sum of the whole array
+        int two=sum-first; // min score of player -2
+        return (first>=two);
+    }
+};
