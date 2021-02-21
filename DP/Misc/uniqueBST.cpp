d46d3d98a6635e08fc36fa29cd130c33e88999c6
Pranav Singhal <iit2019050@iiita.ac.in>
2021-02-21 17:43:33 +0530

    Modified DP

--- DP/Misc/uniqueBST.cpp
+++ DP/Misc/uniqueBST.cpp
@@ -24,13 +24,29 @@
 //      3     2     1      1   3      2
 //     /     /       \                 \
 //    2     1         2                 3
+Lets say you know the answer for values i which ranges from 0 <= i <= n - 1.
+How do you calculate the answer for n.
 
+Lets consider the number [1, n]
+We have n options of choosing the root.
+If we choose the number j as the root, j - 1 numbers fall in the left subtree, n - j numbers fall in the right subtree. We already know how many ways there are to forming j - 1 trees using j - 1 numbers and n -j numbers.
+So we add number(j - 1) * number(n - j) to our solution.
+
+
+int dp[101];
+int solve(int n)
+{
+    if(n==0 || n==1) return 1;
+    if(dp[n]!=-1) return dp[n];
+    int ans=0;
+    for(int i=1;i<=n;i++)
+    {
+        ans+=solve(i-1)*solve(n-i);
+    }
+    return dp[n]=ans;
+}
 int Solution::numTrees(int A) {
-    vector<int> dp(A+1);
-    dp[0] = 1;
-    dp[1] = 1;
-    for(int i=2; i<=A; i++) 
-        for(int j=0; j<i; j++)
-            dp[i] += dp[j]*dp[i-j-1];
-    return dp[A];
+    memset(dp,-1,sizeof(dp));
+    return solve(A);
+    
 }
